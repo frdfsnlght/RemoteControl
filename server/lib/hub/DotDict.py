4ea167e94e52bfe2425a07d9760999ea22e4f63c
frdfsnlght <tab@bennedum.org>
2019-11-25 14:27:13 -0500

    Return DotDict for nested dictionaries

--- server/lib/hub/DotDict.py
+++ server/lib/hub/DotDict.py
@@ -1,8 +1,16 @@
 
 class DotDict(dict):
 
-    __getattr__ = dict.get
+#    __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
     
    
+    def __getattr__(self, k):
+        if k in self:
+            v = self[k]
+            if instanceof(v, dict) and not instanceof(v, DotDict):
+                v = DotDict(v)
+                self[k] = v
+            return v
+        return super().__getattr__(k)
